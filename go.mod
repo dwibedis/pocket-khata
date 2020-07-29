af5ae261d69989dc53b1b2f5f57c56a8d1ceb59a
satyaprakashzomato <satyaprakash.dwibedi@zomato.com>
2020-07-29 17:26:03 +0530

    update

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/pocket-khata
-
-go 1.14
-
-require github.com/gin-gonic/gin v1.6.3
--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-
+	//"github.com/dwibedis/controllers"
 	"log"
 )
 
 func main() {
 	r := gin.Default()
+	//r.GET("/", controllers.)
 	err := r.Run("localhost:8080")
 	if err != nil {
 		log.Panic("Server up failed!!")
